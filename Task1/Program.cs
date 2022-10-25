fd1c4faa7e66c2a2834066affe0f9ef293550a9f
Polina <Polina_piter88@mail.ru>
2022-10-25 09:50:00 +0300

    1 задача

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+bool stop = false;
+int number = 0;
+int summ = 0;
+
+while(!stop)
+{
+    Console.WriteLine("введите число");
+    number = int.Parse(Console.ReadLine());
+   
+    if(number>0)
+    summ++;
+
+    Console.WriteLine("Продолжить вводить числа? y/n");
+    string result = Console.ReadLine();
+    if(result == "y")
+        stop = false;
+    else
+        stop = true;
+}
+Console.WriteLine($"Количество чисел больше 0 - {summ}");
